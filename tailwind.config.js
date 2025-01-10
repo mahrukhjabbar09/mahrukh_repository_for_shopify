add5e5e6b95ef7f0efbe4ca0d79207db3cfd57b0
Mahrukh <mahrukhjabbar607@gmail.com>
2025-01-10 10:53:53 +0500

    Your commit message here

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./layout/*.liquid"],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {
+      height: {
+        '94': '22rem'
+      }
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
